3558b000a3aa80dc2446764f5e4005864ce22c6c
alifmukhlis06-svg <alifmukhlis06@gmail.com>
2025-09-03 20:12:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function changeSlide() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}
+
+setInterval(changeSlide, 3000); // Change image every 3 seconds
